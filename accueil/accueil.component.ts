83297ba3e487d3d4900cd2780f872f4c029b4d47
CovIsma2 <87241077+CovIsma2@users.noreply.github.com>
2021-10-23 16:40:28 -0700

    Add files via upload

---
+++ accueil/accueil.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil',
+  templateUrl: './accueil.component.html',
+  styles: [
+  ]
+})
+export class AccueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
